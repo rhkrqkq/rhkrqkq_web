e63b89fb362f96f9069519c01833b0467aaafb4a
rhkrqkq <rhkrqkq1025@gmail.com>
2024-07-30 15:01:35 +0900

    수정,삭제기능 추가

---
+++ src/main/java/com/mysite/sbb/question/Question.java
@@ -0,0 +1,37 @@
+package com.mysite.sbb.question;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import com.mysite.sbb.answer.Answer;
+import com.mysite.sbb.user.SiteUser;
+import jakarta.persistence.*;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.autoconfigure.task.TaskExecutionProperties;
+
+@Getter
+@Setter
+@Entity
+public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 200)
+    private String subject;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate;
+
+    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
+    private List<Answer> answerList;
+
+    @ManyToOne
+    private SiteUser author;
+
+    private LocalDateTime modifyDate;
+}
